980e096ac401a756d2648fea7b9a1be885bde103
thuviengithub0208 <khoaphp@yahoo.com>
2018-05-16 20:35:46 +0700

    "update"

--- app/src/main/java/com/ptp/phamtanphat/asyntask1903/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/asyntask1903/MainActivity.java
@@ -3,63 +3,71 @@ package com.ptp.phamtanphat.asyntask1903;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txt;
+    Button btnDownload;
+    SeekBar seekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txt = findViewById(R.id.textview);
-        new Thucthiindulieu().execute("Cong viec");
+        btnDownload = findViewById(R.id.button);
+        seekBar = findViewById(R.id.seekbar);
+
+        seekBar.setVisibility(View.GONE);
+
+        btnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thucthidownload().execute();
+            }
+        });
+
     }
     //Tham so 1 : Gia tri truyen vao
     //Tham so 2 : Gia tri update len cho giao dien khi xu ly logic
     //Tham so 3 : Gia tri sau khi xu ly xong logic se tra ve
-    class Thucthiindulieu extends AsyncTask<String,String,String>{
+    class Thucthidownload extends AsyncTask<Void,Integer,String>{
 
         @Override
         protected void onPreExecute() {
-            txt.setText("Bat dau cong viec" + "\n");
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            btnDownload.setVisibility(View.INVISIBLE);
+            seekBar.setVisibility(View.VISIBLE);
             super.onPreExecute();
         }
 
-        //Xu ly logic khong cap nhat truc tiep len view
         @Override
-        protected String doInBackground(String... strings) {
-            for (int i = 0 ; i <4 ; i++){
-                //Cong viec thu 1
-                //Cong viec thu 2
-                //Cong viec thu 3
-                String chuoi = strings[0] + " thu : " + i + "\n";
+        protected String doInBackground(Void... voids) {
+            for (int i = 0 ; i < 5 ; i++){
+                publishProgress(20 );
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                publishProgress(chuoi);
+//                publishProgress((i + 1) * 20);
             }
-            return null;
+            return "Down load ket thuc";
         }
 
         @Override
-        protected void onProgressUpdate(String... values) {
-            txt.append(values[0]);
+        protected void onProgressUpdate(Integer... values) {
+            seekBar.setProgress(seekBar.getProgress() + values[0]);
             super.onProgressUpdate(values);
         }
 
         @Override
         protected void onPostExecute(String s) {
+            seekBar.setVisibility(View.GONE);
+            btnDownload.setVisibility(View.VISIBLE);
+            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
             super.onPostExecute(s);
         }
-
-
     }
 }
